5b6246dbf1b3d08ba02fc1c3087fc26dddad3bb4
wiliami <wiliaup97@gmail.com>
2022-06-12 03:32:14 -0400

    add firebase

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJg0W-Mwf_3WzeuohQv4rM-nsAqnwdRBU",
+    authDomain: "fir-turtle-53625.firebaseapp.com",
+    projectId: "fir-turtle-53625",
+    storageBucket: "fir-turtle-53625.appspot.com",
+    messagingSenderId: "901500946738",
+    appId: "1:901500946738:web:ae91d4fd865ecda3bc2f2b",
+    measurementId: "G-3KJRHXEKCR"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
